6d17784b401d7a942a2e54c50684ce0337511d6c
Midionos <midionos@laposte.net>
2013-02-24 12:13:16 +0100

    Properties of graphs.

---
+++ src/main/Properties.java
@@ -0,0 +1,132 @@
+package main;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ * Methods to check the different properties of graphs.
+ * Works only with 1-graphs.
+ * @author Paul Chaignon
+ */
+public class Properties {
+
+	/**
+	 * Check if a graph is reflexive.
+	 * @param graph The graph.
+	 * @return True if the graph is reflexive, false else.
+	 */
+	public static boolean isReflexive(Graph<Integer, DefaultEdge> graph) {
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertex: vertexes) {
+			if(!graph.containsEdge(vertex, vertex)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Check if a graph is anti-reflexive.
+	 * @param graph The graph.
+	 * @return True if the graph is anti-reflexive, false else.
+	 */
+	public static boolean isAntiReflexive(Graph<Integer, DefaultEdge> graph) {
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertex: vertexes) {
+			if(graph.containsEdge(vertex, vertex)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Check if a graph is symetric.
+	 * @param graph The graph.
+	 * @return True if the graph is symetric, false else.
+	 */
+	public static boolean isSymetric(Graph<Integer, DefaultEdge> graph) {
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				if(graph.containsEdge(vertexX, vertexY)) {
+					if(!vertexX.equals(vertexY) && !graph.containsEdge(vertexY, vertexX)) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Check if a graph is anti-symetric.
+	 * @param graph The graph.
+	 * @return True if the graph is anti-symetric, false else.
+	 */
+	public static boolean isAntiSymetric(Graph<Integer, DefaultEdge> graph) {
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				if(graph.containsEdge(vertexX, vertexY)) {
+					if(!vertexX.equals(vertexY) && graph.containsEdge(vertexY, vertexX)) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Check if a graph is transitive.
+	 * @param graph The graph.
+	 * @return True if the graph is transitive, false else.
+	 */
+	public static boolean isTransitive(Graph<Integer, DefaultEdge> graph) {
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				for(Integer vertexZ: vertexes) {
+					if(graph.containsEdge(vertexX, vertexY) && graph.containsEdge(vertexY, vertexZ)) {
+						if(!graph.containsEdge(vertexX, vertexZ)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Check if a graph is anti-transitive.
+	 * @param graph The graph.
+	 * @return True if the graph is anti-transitive, false else.
+	 */
+	public static boolean isAntiTransitive(Graph<Integer, DefaultEdge> graph) {
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				for(Integer vertexZ: vertexes) {
+					if(graph.containsEdge(vertexX, vertexY) && graph.containsEdge(vertexY, vertexZ)) {
+						if(graph.containsEdge(vertexX, vertexZ)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Check if a graph is strongly anti-symetric.
+	 * @param graph The graph.
+	 * @return True if the graph is strongly anti-symetric, false else.
+	 */
+	public static boolean isStronglyAntiTransitive(Graph<Integer, DefaultEdge> graph) {
+		// TODO No idea on how to check this...
+		return true;
+	}
+}
---
+++ src/test/TestProperties.java
@@ -0,0 +1,180 @@
+package test;
+
+import main.Operations;
+import main.Properties;
+import main.Tools;
+
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.Graph;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests of the methods from Properties.
+ * @author Paul Chaignon
+ */
+public class TestProperties extends TestCase {
+	private Graph<Integer, DefaultEdge> graph1;
+	private Graph<Integer, DefaultEdge> graph2;
+	private Graph<Integer, DefaultEdge> graph3;
+	private Graph<Integer, DefaultEdge> graph4;
+	private Graph<Integer, DefaultEdge> graph5;
+	private Graph<Integer, DefaultEdge> graph6;
+	private Graph<Integer, DefaultEdge> graph7;
+	private Graph<Integer, DefaultEdge> graph8;
+	private Graph<Integer, DefaultEdge> graph9;
+	private Graph<Integer, DefaultEdge> graph10;
+	
+	/**
+	 * Initalize the tests with the graph from the handout.
+	 */
+	protected void setUp() {
+		this.graph1 = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		this.graph2 = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		this.graph4 = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		this.graph5 = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		this.graph8 = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		this.graph10 = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+
+		this.graph1.addVertex(1);
+		this.graph1.addVertex(2);
+		this.graph1.addVertex(3);
+		this.graph1.addVertex(4);
+		this.graph1.addVertex(5);
+		
+		this.graph2.addVertex(1);
+		this.graph2.addVertex(2);
+		this.graph2.addVertex(3);
+		this.graph2.addVertex(4);
+		this.graph2.addVertex(5);
+
+		this.graph1.addEdge(1, 2);
+		this.graph1.addEdge(2, 3);
+		this.graph1.addEdge(2, 4);
+		this.graph1.addEdge(3, 2);
+		this.graph1.addEdge(5, 5);
+		this.graph1.addEdge(1, 1);
+		this.graph1.addEdge(2, 2);
+		this.graph1.addEdge(3, 3);
+		this.graph1.addEdge(4, 4);
+		
+		this.graph2.addEdge(1, 3);
+		this.graph2.addEdge(2, 1);
+		this.graph2.addEdge(2, 3);
+		this.graph2.addEdge(3, 4);
+		this.graph2.addEdge(4, 5);
+		this.graph2.addEdge(5, 2);
+		
+		this.graph3 = Tools.clone(this.graph2);
+		this.graph3.addEdge(3, 3);
+		this.graph3.addEdge(2, 2);
+		
+		this.graph4.addVertex(1);
+		this.graph4.addVertex(2);
+		this.graph4.addVertex(3);
+		
+		this.graph5.addVertex(1);
+		this.graph5.addVertex(2);
+		this.graph5.addVertex(3);
+
+		this.graph4.addEdge(1, 1);
+		this.graph4.addEdge(2, 3);
+		this.graph4.addEdge(3, 2);
+
+		this.graph5.addEdge(1, 1);
+		this.graph5.addEdge(1, 2);
+		this.graph5.addEdge(1, 3);
+		this.graph5.addEdge(2, 3);
+	
+		this.graph6 = Operations.union(this.graph4, this.graph5);
+		
+		this.graph7 = Tools.clone(this.graph6);
+		this.graph7.addEdge(2, 2);
+		this.graph7.addEdge(3, 3);
+		
+		this.graph8.addVertex(1);
+		this.graph8.addVertex(2);
+		this.graph8.addVertex(3);
+
+		this.graph8.addEdge(1, 2);
+		this.graph8.addEdge(2, 3);
+		this.graph8.addEdge(3, 2);
+
+		this.graph9 = Tools.clone(this.graph8);
+		this.graph9.addEdge(1, 1);
+		this.graph9.addEdge(3, 3);
+		
+		this.graph10.addVertex(1);
+		this.graph10.addVertex(2);
+		this.graph10.addVertex(3);
+		this.graph10.addVertex(4);
+
+		this.graph10.addEdge(1, 2);
+		this.graph10.addEdge(2, 3);
+		this.graph10.addEdge(4, 1);
+		this.graph10.addEdge(4, 3);
+	}
+	
+	/**
+	 * Test the isReflexive method.
+	 */
+	public void testIsReflexive() {
+		assertTrue(Properties.isReflexive(this.graph1));
+		assertFalse(Properties.isReflexive(this.graph2));
+		assertFalse(Properties.isReflexive(this.graph3));
+	}
+	
+	/**
+	 * Test the isAntiReflexive method.
+	 */
+	public void testIsAntiReflexive() {
+		assertFalse(Properties.isAntiReflexive(this.graph1));
+		assertTrue(Properties.isAntiReflexive(this.graph2));
+		assertFalse(Properties.isAntiReflexive(this.graph3));
+	}
+	
+	/**
+	 * Test the isSymetric method.
+	 */
+	public void testIsSymetric() {
+		assertTrue(Properties.isSymetric(this.graph4));
+		assertFalse(Properties.isSymetric(this.graph5));
+		assertFalse(Properties.isSymetric(this.graph6));
+	}
+	
+	/**
+	 * Test the isReflexive method.
+	 */
+	public void testIsAntiSymetricReflexive() {
+		assertFalse(Properties.isAntiSymetric(this.graph4));
+		assertTrue(Properties.isAntiSymetric(this.graph5));
+		assertFalse(Properties.isAntiSymetric(this.graph6));
+	}
+	
+	/**
+	 * Test the isTransitive method.
+	 */
+	public void testIsTransitive() {
+		assertTrue(Properties.isTransitive(this.graph7));
+		assertFalse(Properties.isTransitive(this.graph8));
+		assertFalse(Properties.isTransitive(this.graph9));
+	}
+	
+	/**
+	 * Test the isAntiTransitive method.
+	 */
+	public void testIsAntiTransitive() {
+		assertFalse(Properties.isAntiTransitive(this.graph7));
+		assertTrue(Properties.isAntiTransitive(this.graph8));
+		assertFalse(Properties.isAntiTransitive(this.graph9));
+	}
+	
+	/**
+	 * Test the isStronglyAntiTransitive method.
+	 */
+	public void testIsStronglyAntiTransitive() {
+		fail("Not implemented yet...");
+		assertFalse(Properties.isStronglyAntiTransitive(this.graph10));
+	}
+}
