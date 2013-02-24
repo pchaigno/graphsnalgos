eaa54ff2dfefe655e5ca54da89a47be8ac2e4902
Midionos <midionos@laposte.net>
2013-02-24 12:12:36 +0100

    Operations on graphs.

---
+++ src/main/Operations.java
@@ -0,0 +1,192 @@
+package main;
+
+import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ * Methods to get the different operations on graphs.
+ * Works only with 1-graphs.
+ * @author Paul Chaignon
+ */
+public class Operations {
+
+	/**
+	 * Construct the union of two graphs.
+	 * The result graph is always a 1-graph.
+	 * The two graph must have the same vertexes.
+	 * @param graph1 The first graph.
+	 * @param graph2 The second graph.
+	 * @return The union of graph1 and graph2.
+	 * @throws IllegalArgumentException
+	 */
+	public static Graph<Integer, DefaultEdge> union(Graph<Integer, DefaultEdge> graph1, Graph<Integer, DefaultEdge> graph2) throws IllegalArgumentException {
+		Graph<Integer, DefaultEdge> graph = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		Integer[] vertexes = graph1.vertexSet().toArray(new Integer[0]);
+		Integer[] vertexes2 = graph2.vertexSet().toArray(new Integer[0]);
+		if(!Tools.arrayEquals(vertexes, vertexes2)) {
+			throw new IllegalArgumentException("The two graphs must have the same vertexes.");
+		}
+		for(Integer vertex: vertexes) {
+			graph.addVertex(vertex);
+		}
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				if(graph1.containsEdge(vertexX, vertexY) || graph2.containsEdge(vertexX, vertexY)) {
+					graph.addEdge(vertexX, vertexY);
+				}
+			}
+		}
+		return graph;
+	}
+	
+	/**
+	 * Construct the composition of two graphs.
+	 * The result graph is always a 1-graph.
+	 * @param graph1 The first graph.
+	 * @param graph2 The second graph.
+	 * @return The composition of graph1 and graph2, graph1 o graph2.
+	 * @throws IllegalArgumentException
+	 */
+	public static Graph<Integer, DefaultEdge> composition(Graph<Integer, DefaultEdge> graph1, Graph<Integer, DefaultEdge> graph2) throws IllegalArgumentException {
+		Graph<Integer, DefaultEdge> graph = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		Integer[] vertexes = graph1.vertexSet().toArray(new Integer[0]);
+		Integer[] vertexes2 = graph2.vertexSet().toArray(new Integer[0]);
+		if(!Tools.arrayEquals(vertexes, vertexes2)) {
+			throw new IllegalArgumentException("The two graphs must have the same vertexes.");
+		}
+		for(Integer vertex: vertexes) {
+			graph.addVertex(vertex);
+		}
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				for(Integer vertexZ: vertexes) {
+					if(graph2.containsEdge(vertexX, vertexZ) && graph1.containsEdge(vertexZ, vertexY)) {
+						graph.addEdge(vertexX, vertexY);
+					}
+				}
+			}
+		}
+		return graph;
+	}
+	
+	/**
+	 * Construct the graph power p.
+	 * The result graph is always a 1-graph.
+	 * This method is recursive (non-tail-recursive).
+	 * @param graph The graph.
+	 * @param p The power.
+	 * @return Power p of the graph
+	 */
+	public static Graph<Integer, DefaultEdge> power(Graph<Integer, DefaultEdge> graph, int p) {
+		if(p==1) {
+			return Tools.clone(graph);
+		}
+		return composition(power(graph, p-1), graph);
+	}
+	
+	/**
+	 * Construct the transposed graph.
+	 * @param graph The graph.
+	 * @return The transposed graph.
+	 */
+	public static Graph<Integer, DefaultEdge> transpose(Graph<Integer, DefaultEdge> graph) {
+		Graph<Integer, DefaultEdge> transposed = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertex: vertexes) {
+			transposed.addVertex(vertex);
+		}
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				if(graph.containsEdge(vertexX, vertexY)) {
+					transposed.addEdge(vertexY, vertexX);
+				}
+			}
+		}
+		return transposed;
+	}
+	
+	/**
+	 * Construct the complementary graph.
+	 * @param graph The graph.
+	 * @return The complementary graph.
+	 */
+	public static Graph<Integer, DefaultEdge> complementary(Graph<Integer, DefaultEdge> graph) {
+		Graph<Integer, DefaultEdge> complementary = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertex: vertexes) {
+			complementary.addVertex(vertex);
+		}
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				if(!graph.containsEdge(vertexX, vertexY)) {
+					complementary.addEdge(vertexX, vertexY);
+				}
+			}
+		}
+		return complementary;
+	}
+	
+	/**
+	 * Construct the complementary graph without the loops.
+	 * @param graph The graph.
+	 * @return The complementary graph whitout loops.
+	 */
+	public static Graph<Integer, DefaultEdge> complementaryWithoutLoops(Graph<Integer, DefaultEdge> graph) {
+		Graph<Integer, DefaultEdge> complementary = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertex: vertexes) {
+			complementary.addVertex(vertex);
+		}
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				if(!vertexX.equals(vertexY) && !graph.containsEdge(vertexX, vertexY)) {
+					complementary.addEdge(vertexX, vertexY);
+				}
+			}
+		}
+		return complementary;
+	}
+	
+	/**
+	 * Check if a graph is the partial graph of another.
+	 * @param partialGraph The possible partial graph to test.
+	 * @param graph The graph.
+	 * @return True if partialGraph is the partial graph of graph.
+	 */
+	public static boolean isPartialGraph(Graph<Integer, DefaultEdge> partialGraph, Graph<Integer, DefaultEdge> graph) {
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				if(partialGraph.containsEdge(vertexX, vertexY) && !graph.containsEdge(vertexX, vertexY)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Construct the subgraph of graph from a.
+	 * @param graph The original graph.
+	 * @param a The list of vertex for the subgraph.
+	 * @return The subgraph of graph from a.
+	 */
+	public static Graph<Integer, DefaultEdge> subgraph(Graph<Integer, DefaultEdge> graph, List<Integer> a) {
+		Graph<Integer, DefaultEdge> subgraph = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		Integer[] vertexes = graph.vertexSet().toArray(new Integer[0]);
+		for(Integer vertex: a) {
+			subgraph.addVertex(vertex);
+		}
+		for(Integer vertexX: vertexes) {
+			for(Integer vertexY: vertexes) {
+				if(a.contains(vertexX) && a.contains(vertexY) && graph.containsEdge(vertexX, vertexY)) {
+					subgraph.addEdge(vertexX, vertexY);
+				}
+			}
+		}
+		return subgraph;
+	}
+}
---
+++ src/test/TestOperations.java
@@ -0,0 +1,143 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.Operations;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests of the methods from Operations.
+ * @author Paul Chaignon
+ */
+public class TestOperations extends TestCase {
+	private Graph<Integer, DefaultEdge> graph1;
+	private Graph<Integer, DefaultEdge> graph2;
+
+	/**
+	 * Initalize the tests with the graph from the handout.
+	 */
+	protected void setUp() throws Exception {
+		this.graph1 = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+		this.graph2 = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+
+		this.graph1.addVertex(1);
+		this.graph1.addVertex(2);
+		this.graph1.addVertex(3);
+		this.graph1.addVertex(4);
+		this.graph1.addVertex(5);
+		
+		this.graph2.addVertex(1);
+		this.graph2.addVertex(2);
+		this.graph2.addVertex(3);
+		this.graph2.addVertex(4);
+		this.graph2.addVertex(5);
+
+		this.graph1.addEdge(1, 2);
+		this.graph1.addEdge(2, 3);
+		this.graph1.addEdge(2, 4);
+		this.graph1.addEdge(3, 2);
+		this.graph1.addEdge(5, 5);
+		
+		this.graph2.addEdge(1, 3);
+		this.graph2.addEdge(2, 1);
+		this.graph2.addEdge(2, 3);
+		this.graph2.addEdge(3, 4);
+		this.graph2.addEdge(4, 5);
+		this.graph2.addEdge(5, 2);
+	}
+	
+	/**
+	 * Test the union method.
+	 */
+	public void testUnion() {
+		Graph<Integer, DefaultEdge> graph = Operations.union(this.graph1, this.graph2);
+		System.out.println("Union:");
+		System.out.println(graph);
+	}
+	
+	/**
+	 * Test the composition method.
+	 */
+	public void testComposition() {
+		Graph<Integer, DefaultEdge> graph = Operations.composition(this.graph1, this.graph2);
+		System.out.println("Composition g1 o g2:");
+		System.out.println(graph);
+		graph = Operations.composition(this.graph2, this.graph1);
+		System.out.println("Composition g2 o g1:");
+		System.out.println(graph);
+	}
+	
+	/**
+	 * Test the power method.
+	 */
+	public void testPower() {
+		Graph<Integer, DefaultEdge> graph = Operations.power(this.graph1, 2);
+		System.out.println("g1²:");
+		System.out.println(graph);
+	}
+	
+	/**
+	 * Test the transpose method.
+	 */
+	public void testTransposed() {
+		Graph<Integer, DefaultEdge> graph = Operations.transpose(this.graph1);
+		System.out.println("g1 transposed:");
+		System.out.println(graph);
+		graph = Operations.transpose(this.graph2);
+		System.out.println("g2 transposed:");
+		System.out.println(graph);
+	}
+	
+	/**
+	 * Test the complementary method.
+	 */
+	public void testComplementary() {
+		Graph<Integer, DefaultEdge> graph = Operations.complementary(this.graph1);
+		System.out.println("g1 complementary:");
+		System.out.println(graph);
+		graph = Operations.complementary(this.graph2);
+		System.out.println("g2 complementary:");
+		System.out.println(graph);
+	}
+	
+	/**
+	 * Test the complementaryWithoutLoops method.
+	 */
+	public void testComplementaryWithoutLoops() {
+		Graph<Integer, DefaultEdge> graph = Operations.complementaryWithoutLoops(this.graph1);
+		System.out.println("g1 complementary without loops:");
+		System.out.println(graph);
+		graph = Operations.complementaryWithoutLoops(this.graph2);
+		System.out.println("g2 complementary without loops:");
+		System.out.println(graph);
+	}
+	
+	/**
+	 * Test the isPartialGraph method.
+	 */
+	public void testIsPartialGraph() {
+		// TODO
+	}
+	
+	/**
+	 * Test the subgraph method.
+	 */
+	public void testSubgraph() {
+		List<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(4);
+		Graph<Integer, DefaultEdge> graph = Operations.subgraph(this.graph1, a);
+		System.out.println("g1 subgraph:");
+		System.out.println(graph);
+		graph = Operations.subgraph(this.graph2, a);
+		System.out.println("g2 subgraph:");
+		System.out.println(graph);
+	}
+}
