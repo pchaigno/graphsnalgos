1afe55247f5e3437097674e1b1f350fc40fea2a6
Paul Chaignon <midionos@laposte.net>
2013-04-03 21:00:38 +0200

    Test of Graph improved.

--- src/test/TestGraph.java
+++ src/test/TestGraph.java
@@ -8,6 +8,7 @@ public class TestGraph extends TestCase {
 	private Graph graph1;
 	private Graph graph2;
 	private Graph graph3;
+	private Graph graph4;
 	
 	protected void setUp() {
 		this.graph1 = new DefaultGraph();
@@ -23,15 +24,21 @@ public class TestGraph extends TestCase {
 		this.graph3 = new DefaultGraph();
 		this.graph3.addVertex(1);
 		this.graph3.addVertex(2);
-		this.graph3.addVertex(3);
-		this.graph3.addEdge(1, 2);
-		this.graph3.addEdge(2, 3);
-		this.graph3.addEdge(3, 1);
+		this.graph3.addEdge(2, 1);
+		
+		this.graph4 = new DefaultGraph();
+		this.graph4.addVertex(1);
+		this.graph4.addVertex(2);
+		this.graph4.addVertex(3);
+		this.graph4.addEdge(1, 2);
+		this.graph4.addEdge(2, 3);
+		this.graph4.addEdge(3, 1);
 	}
 	
 	public void testEquals() {
 		assertEquals(this.graph1, this.graph2);
-		assertFalse(this.graph1.equals(this.graph3));
+		assertEquals(this.graph1, this.graph3);
+		assertFalse(this.graph1.equals(this.graph4));
 	}
 	
 	public void testClone() {
