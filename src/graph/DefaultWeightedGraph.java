45384936687ce8dc91b0d4eb47321fc3db20d826
Paul Chaignon <midionos@laposte.net>
2013-04-06 13:03:10 +0200

    Error in WeightedGraph with WeightedEdge corrected.

--- src/graph/DefaultWeightedGraph.java
+++ src/graph/DefaultWeightedGraph.java
@@ -15,9 +15,19 @@ public class DefaultWeightedGraph extends DefaultDirectedGraph implements Weight
 	public boolean addEdge(int x, int y, double value) {
 		return this.addEdge(new DefaultWeightedEdge(x, y, value));
 	}
+
+	@Override
+	public boolean removeEdge(int x, int y) {
+		return this.edges.remove(new DefaultWeightedEdge(x, y));
+	}
 	
 	@Override
 	public DefaultWeightedGraph clone() {
 		return (DefaultWeightedGraph)super.clone();
+	}
+
+	@Override
+	public boolean containsEdge(int x, int y) {
+		return this.edges.contains(new DefaultWeightedEdge(x, y));
 	}
 }
