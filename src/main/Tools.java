0889fa303829a7479a3bfc1799dbb46be9f11180
Paul Chaignon <midionos@laposte.net>
2013-04-08 20:22:16 +0200

    Polymorphic method matrixToString added.

--- src/main/Tools.java
+++ src/main/Tools.java
@@ -27,6 +27,27 @@ public class Tools {
 	}
 	
 	/**
+	 * @param matrix The matrix.
+	 * @return A string representation of an integer matrix.
+	 */
+	public static String matrixToString(double[][] matrix) {
+		String str = "";
+		int length_matrix = matrix.length;
+		int length_vector;
+		for(int i=0 ; i<length_matrix ; i++) {
+			length_vector = matrix[i].length;
+			for(int j=0 ; j<length_vector ; j++) {
+				if(matrix[i][j]>=0) {
+					str += " ";
+				}
+				str += matrix[i][j]+" ";
+			}
+			str += "\n";
+		}
+		return str;
+	}
+	
+	/**
 	 * Build a copy of a set of integers.
 	 * @param set The set to clone.
 	 * @return The cloned set.
